b5da99f3ece56641158ed9d90bc52fcc41e1e876
Mustafa Haddad <79608345+Mustafa877@users.noreply.github.com>
2024-04-24 16:06:46 +0300

    updates NavBar

--- src/App.vue
+++ src/App.vue
@@ -46,12 +46,23 @@ const currentView = computed(() => {
 const isActive = (path) => {
   return currentPath.value === '#' + path
 }
+const isMenuOpen = ref(false)
+
+const toggleMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value
+  
+}
 
 </script>
 
 <template>
   <nav class="navbar">
-    <ul class="navbar-nav">
+    <div class="menu-icon" @click="toggleMenu">
+      <div class="bar"></div>
+      <div class="bar"></div>
+      <div class="bar"></div>
+    </div>
+    <ul class="navbar-nav" :class="{ 'active': isMenuOpen }">
       <li :class="{ active: isActive('/') }"><a href="#/" class="nav-item home">Home</a></li>
       <li :class="{ active: isActive('/AmazingCalculator') }"><a href="#/AmazingCalculator" class="nav-item calculator">AmazingCalculator</a></li>
       <li :class="{ active: isActive('/PasswordGenerator') }"><a href="#/PasswordGenerator" class="nav-item generator">PasswordGenerator</a></li>
@@ -67,6 +78,8 @@ const isActive = (path) => {
   </nav>
   <component :is="currentView" />
 </template>
+
+ 
 
 <style scoped>
 .navbar {
@@ -101,7 +114,6 @@ const isActive = (path) => {
   background-color: #555;
 }
 
- 
 .home {
   background-color: #007bff;
 }
@@ -154,31 +166,135 @@ const isActive = (path) => {
   background-color: #000000;
   color: #ffffff !important;  
 }
-
-@media (max-width: 768px) {
-  .navbar {
-    flex-direction: column;
-    
-  }
-
-  .navbar-nav {
-    
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-
-  .navbar-nav li {
-    margin: 5px 0;
-    width: 100%;
-  }
-
-  .navbar-nav li a {
-    display: block;
-    width: 100%;
-    padding: 8px 0;
-    text-align: center;
-  }
+.menu-icon {
+  display: none;
+  cursor: pointer;
 
 }
+
+.bar {
+  width: 25px;
+  height: 3px;
+  margin-bottom: 4px;
+  background-color: #000000;
+  transition: background-color 0.3s ease;
+}
+
+.menu-icon:hover .bar {
+  background-color: #ccc;
+}
+
+.navbar-nav {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+}
+
+.navbar-nav li {
+  display: inline-block;
+  margin-right: 10px;
+}
+
+.navbar-nav li a {
+  text-decoration: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  transition: background-color 0.3s ease;
+  color: #fff;
+  font-size: 16px;
+  font-weight: bold;
+}
+
+.navbar-nav li a:hover {
+  background-color: #555;
+}
+
+.active a {
+  background-color: #000000e1;
+}
+
+.active a:hover {
+  background-color: #777;
+}
+
+ 
+@media (max-width: 1690px) {
+  .menu-icon {
+    display: block;
+  }
+  
+  .navbar-nav {
+    display: none;
+    position: absolute;
+    top: 80px;
+    width: auto;
+    background-color: #444;
+    padding: 10px;
+    border-radius: 8px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+    z-index: 999;
+    height: 100%;
+ 
+  }
+  
+  .navbar-nav.active {
+    display: block;
+  }
+ 
+  .navbar-nav li {
+    display: block;
+    margin: 10px 0;
+  }
+  
+  .navbar-nav li a {
+    display: block;
+    padding: 10px 0;
+    border-bottom: 1px solid #666;
+  }
+  .home {
+  background-color: #007bff;
+}
+
+.calculator {
+  background-color: #28a745;
+}
+
+.generator {
+  background-color: #ffc107;
+}
+
+.accordion {
+  background-color: #17a2b8;
+}
+
+.validation {
+  background-color: #dc3545;
+}
+
+.progress {
+  background-color: #6f42c1;
+}
+
+.github {
+  background-color: #e65c00;
+}
+
+.todo {
+  background-color: #17a2b8;
+}
+
+.jokes {
+  background-color: #ffc107;
+}
+
+.quote {
+  background-color: #28a745;
+}
+
+.wiki {
+  background-color: #007bff;
+}
+}
+ 
+
 </style>
